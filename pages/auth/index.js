84a6e9f3ff23f78685a515f33259cdac7c310df4
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-17 22:52:36 -0200

    Using User component in the auth page.

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import User from '../../components/User'
 
 const authIndexPage = () => (
   <div>
     <h1>The Auth Page</h1>
     <p>Go back <Link href="/">Home</Link></p>
+    <User name="Anderson Cossul" age="19" />
   </div>
 )
 
