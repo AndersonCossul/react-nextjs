c5faa78ecbea2c193b9a02cb818d670d4e2d474c
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-17 22:48:43 -0200

    Navigating programatically.

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Link from 'next/link'
 
 const authIndexPage = () => (
   <div>
     <h1>The Auth Page</h1>
+    <p>Go back <Link href="/">Home</Link></p>
   </div>
 )
 
