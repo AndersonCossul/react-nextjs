47b801aff75b37caabf7d6fe07f965ade83abb69
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-19 20:09:14 -0300

    Dynamic urls.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Layout from '../components/layout/master'
+import PostLink from '../components/PostLink/PostLink'
 
 const Index = () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="SSR for the win" />
+    </ul>
   </Layout>
 )
 
